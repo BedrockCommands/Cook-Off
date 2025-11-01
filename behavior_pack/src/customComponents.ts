5d4cf12fce7afb0a5767dbe729a96ccee4d6e99c
BrodBlox09 <brodenskicomp@outlook.com>
2025-11-01 17:39:56 +0100

    Follow programming best practices
    Replace == and let with === and const when applicable.

--- behavior_pack/src/customComponents.ts
+++ behavior_pack/src/customComponents.ts
@@ -20,27 +20,31 @@ function pickupableInteract(event: BlockComponentPlayerInteractEvent) {
 }
 
 function fryingPanInteract(event: BlockComponentPlayerInteractEvent) {
-	let player = event.player;
-	let inventory = new PlayerInventory(player);
-	if (!inventory.getSelectedItem()) {
+	const player = event.player;
+	const inventory = new PlayerInventory(player);
+	const selectedSlot = inventory.getSelectedSlot();
+	if (!selectedSlot.hasItem()) {
 		// If player's hand is empty, pick up frying pan
 		pickupableInteract(event);
 		return;
 	}
-	let block = event.block;
+	const selectedItem = selectedSlot.getItem();
+	const block = event.block;
+	// When implemented: Add the currently selected item to the frying pan and remove it from the player
 }
 
 function trashBinInteract(event: BlockComponentPlayerInteractEvent) {
-	let player = event.player;
-	let playerInventoryContainer = player.getComponent("minecraft:inventory").container;
-	let selectedSlotIndex = player.selectedSlotIndex;
-	let selectedSlot = playerInventoryContainer.getSlot(selectedSlotIndex);
+	const player = event.player;
+	const playerInventoryContainer = player.getComponent("minecraft:inventory").container;
+	const selectedSlotIndex = player.selectedSlotIndex;
+	const selectedSlot = playerInventoryContainer.getSlot(selectedSlotIndex);
 	// Make sure the player has an item in their hand
-	let selectedItem = selectedSlot.getItem();
+	const selectedItem = selectedSlot.getItem();
 	if (!selectedItem) return;
 
-	if (selectedItem.typeId == "bcc.cook:frying_pan") {
-		
+	if (selectedItem.typeId === "bcc.cook:frying_pan") {
+		// Keep the frying pan, but clear the contents of it
+		// This behavior may need to be generalized to any item that can hold food
 	}
 	else selectedSlot.setItem(); // Clear slot
 	SoundManager.playSound("block.decorated_pot.insert", event.block.location, {
@@ -49,7 +53,7 @@ function trashBinInteract(event: BlockComponentPlayerInteractEvent) {
 }
 
 system.beforeEvents.startup.subscribe(event => {
-	let bcr = event.blockComponentRegistry;
+	const bcr = event.blockComponentRegistry;
 	// bcr.registerCustomComponent("bcc.cook:pickupable", {
 	// 	onPlayerInteract: pickupableInteract
 	// });
