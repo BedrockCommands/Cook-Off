7f197a8ffe9651c9e9cc6dd2d2763b5c76b8dec0
BrodBlox09 <brodenskicomp@outlook.com>
2025-11-26 21:59:25 +0100

    Update frying pan functionality
    Frying pan can now have fryable items deposited into it. When picked up, the information of these items stay tracked on the frying pan item. When implemented, the player will be able to see this information. For now, admin tools should be used.

--- behavior_pack/scripts/components/blocks/fryingPan.js
+++ behavior_pack/scripts/components/blocks/fryingPan.js
@@ -2,32 +2,49 @@
 // Copyright (c) @brodblox09 & Contributors
 // Contributors: @brodblox09 @jeanmajid
 // See LICENSE.md file in the root folder, licenses/MIT.md, or https://opensource.org/license/mit
+import { BlockDataManager } from "../../blockData/blockDataManager";
 import { ComponentManager } from "../componentManager";
 import PlayerInventory from "../../PlayerInventory";
 import SoundManager from "../../SoundManager";
+import { getDefaultFryingPanBlockData } from "../../blockData/fryingPan";
 ComponentManager.registerBlockComponent("bcc.cook:frying_pan" /* BlockId.fryingPan */, {
     onPlayerInteract: (event) => {
         const player = event.player;
+        const block = event.block;
         const inventory = new PlayerInventory(player);
         const selectedSlot = inventory.getSelectedSlot();
         if (!selectedSlot.hasItem()) {
-            // If player's hand is empty, pick up frying pan
-            pickupableInteract(event);
+            pickupFryingPan(player, block);
             return;
         }
-        const selectedItem = selectedSlot.getItem();
-        const block = event.block;
-        // When implemented: Add the currently selected item to the frying pan and remove it from the player
+        addSelectedItemToFryingPan(block, selectedSlot);
     },
+    beforeOnPlayerPlace: (event) => {
+        const player = event.player;
+        const block = event.block;
+        const inventory = new PlayerInventory(player);
+        const blockItemStack = inventory.getSelectedItem();
+        const blockData = BlockDataManager.getBlockDataFromItemStack(blockItemStack, getDefaultFryingPanBlockData());
+        BlockDataManager.setBlockData(block, blockData);
+    }
 });
-function pickupableInteract(event) {
-    const player = event.player;
-    const block = event.block;
-    const blockItemStack = block.getItemStack(1, true);
+function addSelectedItemToFryingPan(block, selectedSlot) {
+    const selectedItem = selectedSlot.getItem();
+    if (!selectedItem.hasTag("bcc.cook:fryable"))
+        return;
+    const blockData = BlockDataManager.getBlockData(block, getDefaultFryingPanBlockData());
+    blockData.items.push(selectedItem.typeId);
+    BlockDataManager.setBlockData(block, blockData);
+    selectedSlot.setItem();
+}
+function pickupFryingPan(player, block) {
+    // block.getItemStack() returns the original block item and not the redefined block item for some reason
+    // and we need the redefined block item since that one is not stackable
+    const blockItemStack = BlockDataManager.getItemFromBlockWithData(block);
     const inventory = new PlayerInventory(player);
     if (block.hasTag("bcc.cook:counter_placeable_in_adventure"))
         blockItemStack.setCanPlaceOn(["minecraft:stone"]); // Replace with bcc.cook:counter once created
     inventory.give(blockItemStack);
     block.setType("minecraft:air");
-    SoundManager.playSound("block.decorated_pot.insert", event.block.location);
+    SoundManager.playSound("block.decorated_pot.insert", block.location);
 }
