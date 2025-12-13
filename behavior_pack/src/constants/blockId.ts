c4724aca42b7278d6866701d6dea01f188804895
BrodBlox09 <brodenskicomp@outlook.com>
2025-12-13 01:51:30 +0100

    Add missing placeholders
    Also fix bowl block definition

--- behavior_pack/src/constants/blockId.ts
+++ behavior_pack/src/constants/blockId.ts
@@ -10,7 +10,6 @@ export const enum BlockId {
     burgerBunsStorageCreate = "bcc.cook:burger_buns_storage_crate",
     pot = "bcc.cook:pot",
     bowl = "bcc.cook:bowl",
-    emptyBowl = "bcc.cook:empty_bowl",
     stove = "bcc.cook:stove",
     oven = "bcc.cook:oven",
     cuttingBoard = "bcc.cook:cutting_board",
